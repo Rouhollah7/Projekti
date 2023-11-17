7b61b6b896c94ee64b433b80ab5dcb956f9a6a27
Rouhollah7 <141823178+Rouhollah7@users.noreply.github.com>
2023-11-17 10:59:34 +0200

    Add files via upload

---
+++ projekti7.js
@@ -0,0 +1,13 @@
+function aani(elain) {
+    aani = elain.id;
+    var audio = new Audio('sounds/' + aani + '.mp3');
+    audio.play();
+    stop();
+
+}
+
+function stop() {
+    console.log("stop");
+    audio.stop();
+}
+
